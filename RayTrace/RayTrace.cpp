bf2f3b29ac7ea2dc01f2ac77764a376a4959075b
finicu212 <smurfdeplastic@gmail.com>
2020-08-16 21:00:56 +0300

    output file will now be a .bmp

--- RayTrace/RayTrace.cpp
+++ RayTrace/RayTrace.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
+#include "../includes/EasyBMP/EasyBMP.h"
 
 int main() {
 
     // Image
 
-    const int image_width = 256;
-    const int image_height = 256;
+    const int IMAGE_WIDTH = 256;
+    const int IMAGE_HEIGHT = 256;
 
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    BMP image;
+    image.SetSize(IMAGE_WIDTH, IMAGE_HEIGHT);
+    image.SetBitDepth(16);
 
-    for (int j = image_height - 1; j >= 0; --j) {
-        for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
+    for (int j = IMAGE_HEIGHT - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < IMAGE_WIDTH; ++i)
+        {
+            RGBApixel pixelCurrent = RGBApixel();
+
+            pixelCurrent.Alpha = 255;
+
+            auto r = double(i) / (IMAGE_WIDTH - 1);
+            auto g = double(j) / (IMAGE_HEIGHT - 1);
             auto b = 0.25;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            pixelCurrent.Red = static_cast<int>(255 * r);
+            pixelCurrent.Green = static_cast<int>(255 * g);
+            pixelCurrent.Blue = static_cast<int>(255 * b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            image.SetPixel(j, i, pixelCurrent);
         }
     }
+
+    image.WriteToFile("out.bmp");
 }
