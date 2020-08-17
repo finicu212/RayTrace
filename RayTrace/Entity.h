e858b14dba1340f0c9c5feeb982c0b8239ad9a02
finicu212 <smurfdeplastic@gmail.com>
2020-08-17 10:34:12 +0300

    simple abstract entity class

---
+++ RayTrace/Entity.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Ray.h"
+
+struct hitInfo
+{
+	Point3 location;
+	Vec3 normal;
+	// t_min < t < t_max
+	double t;
+};
+
+// abstract class
+class Entity
+{
+public:
+	virtual bool isHit(const Ray& r, const hitInfo& info, double t_min, double t_max);
+};
