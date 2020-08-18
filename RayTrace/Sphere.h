e896218753213941fa7ed49656fcf4800be10649
finicu212 <smurfdeplastic@gmail.com>
2020-08-18 15:03:30 +0300

    fixed side effect

--- RayTrace/Sphere.h
+++ RayTrace/Sphere.h
@@ -17,8 +17,6 @@ private:
 
 hitInfo Sphere::isHit(const Ray& r, const hitInfo& info, double t_min, double t_max) const
 {
-	hitInfo returnedInfo();
-
 	Vec3 Len = center - r.getOrigin();
 	double a = r.getDirection().norm() * r.getDirection().norm();
 	double tcb = Len.dot(r.getDirection());
@@ -32,23 +30,27 @@ hitInfo Sphere::isHit(const Ray& r, const hitInfo& info, double t_min, double t_
 		t0 = (-tcb - sqrt(delta)) / a;
 		if (t0 > t_min && t0 < t_max)
 		{
-			info.hitEntity = true;
-			info.location = r.pointAt(t0);
-			info.normal = (r.pointAt(t0) - center) / radius;
-			info.t = t0;
-			return info;
+			hitInfo returnedInfo;
+			returnedInfo.hitEntity = true;
+			returnedInfo.location = r.pointAt(t0);
+			returnedInfo.normal = (r.pointAt(t0) - center) / radius;
+			returnedInfo.t = t0;
+			return returnedInfo;
 		}
 
 		t0 = (-tcb + sqrt(delta)) / a;
 		if (t0 > t_min && t0 < t_max)
 		{
-			info.hitEntity = true;
-			info.location = r.pointAt(t0);
-			info.normal = (r.pointAt(t0) - center) / radius;
-			info.t = t0;
-			return info;
+			hitInfo returnedInfo;
+			returnedInfo.hitEntity = true;
+			returnedInfo.location = r.pointAt(t0);
+			returnedInfo.normal = (r.pointAt(t0) - center) / radius;
+			returnedInfo.t = t0;
+			return returnedInfo;
 		}
 	}
 
-	return false;
+	hitInfo returnedInfo;
+	returnedInfo.hitEntity = false;
+	return returnedInfo;
 }
