a9fca56bcc7a13b946d56b217b77bc3924f4ec81
finicu212 <smurfdeplastic@gmail.com>
2020-08-18 14:49:30 +0300

    added bool to struct

--- RayTrace/Entity.h
+++ RayTrace/Entity.h
@@ -3,13 +3,15 @@
 
 struct hitInfo
 {
+	bool hitEntity;
 	Point3 location;
 	Vec3 normal;
 	// t_min < t < t_max
 	double t;
 
-	void update(Point3 loc, Vec3 n, double t)
+	void update(bool hitSomething, Point3 loc, Vec3 n, double t)
 	{
+		this->hitEntity = hitSomething;
 		this->location = loc;
 		this->normal = n;
 		this->t = t;
