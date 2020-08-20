f358c4940e0be5112c702690e590ee7dcd0d2053
finicu212 <smurfdeplastic@gmail.com>
2020-08-20 20:04:24 +0300

    fixed Camera class fields

--- RayTrace/Camera.h
+++ RayTrace/Camera.h
@@ -6,15 +6,15 @@ class Camera
 public:
     Camera()
     {
-        const double ASPECT_RATIO = 16.0 / 9;
-        const double VIEWPORT_HEIGHT = 2.0;
-        const double VIEWPORT_WIDTH = ASPECT_RATIO * VIEWPORT_HEIGHT;
-        const double FOCAL_LENGTH = 1.0;
+        double ASPECT_RATIO = 16.0 / 9;
+        double VIEWPORT_HEIGHT = 2.0;
+        double VIEWPORT_WIDTH = ASPECT_RATIO * VIEWPORT_HEIGHT;
+        double FOCAL_LENGTH = 1.0;
 
-        const Point3 ORIGIN = Point3(0, 0, 0);
-        const Vec3 HORIZ_VEC = Vec3(VIEWPORT_WIDTH, 0, 0);
-        const Vec3 VERTICAL_VEC = Vec3(0, VIEWPORT_HEIGHT, 0);
-        const Point3 BOTTOM_LEFT = ORIGIN - HORIZ_VEC / 2 - VERTICAL_VEC / 2 - Vec3(0, 0, FOCAL_LENGTH);
+        Point3 ORIGIN = Point3(0, 0, 0);
+        Vec3 HORIZ_VEC = Vec3(VIEWPORT_WIDTH, 0, 0);
+        Vec3 VERTICAL_VEC = Vec3(0, VIEWPORT_HEIGHT, 0);
+        Point3 BOTTOM_LEFT = ORIGIN - HORIZ_VEC / 2 - VERTICAL_VEC / 2 - Vec3(0, 0, FOCAL_LENGTH);
     }
 
     Ray getRay(double u, double v) const
@@ -23,6 +23,11 @@ public:
     }
 
 private:
+    double ASPECT_RATIO;
+    double VIEWPORT_HEIGHT;
+    double VIEWPORT_WIDTH;
+    double FOCAL_LENGTH;
+
     Point3 ORIGIN;
     Point3 BOTTOM_LEFT;
     Vec3 HORIZ_VEC;
