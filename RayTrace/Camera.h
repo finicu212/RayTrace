50930c8a2441e97a4cee3d7e2787cb1630a9f247
finicu212 <smurfdeplastic@gmail.com>
2020-08-20 20:05:44 +0300

    fixed Camera class fields

--- RayTrace/Camera.h
+++ RayTrace/Camera.h
@@ -6,15 +6,15 @@ class Camera
 public:
     Camera()
     {
-        double ASPECT_RATIO = 16.0 / 9;
-        double VIEWPORT_HEIGHT = 2.0;
-        double VIEWPORT_WIDTH = ASPECT_RATIO * VIEWPORT_HEIGHT;
-        double FOCAL_LENGTH = 1.0;
+        ASPECT_RATIO = 16.0 / 9;
+        VIEWPORT_HEIGHT = 2.0;
+        VIEWPORT_WIDTH = ASPECT_RATIO * VIEWPORT_HEIGHT;
+        FOCAL_LENGTH = 1.0;
 
-        Point3 ORIGIN = Point3(0, 0, 0);
-        Vec3 HORIZ_VEC = Vec3(VIEWPORT_WIDTH, 0, 0);
-        Vec3 VERTICAL_VEC = Vec3(0, VIEWPORT_HEIGHT, 0);
-        Point3 BOTTOM_LEFT = ORIGIN - HORIZ_VEC / 2 - VERTICAL_VEC / 2 - Vec3(0, 0, FOCAL_LENGTH);
+        ORIGIN = Point3(0, 0, 0);
+        HORIZ_VEC = Vec3(VIEWPORT_WIDTH, 0, 0);
+        VERTICAL_VEC = Vec3(0, VIEWPORT_HEIGHT, 0);
+        BOTTOM_LEFT = ORIGIN - HORIZ_VEC / 2 - VERTICAL_VEC / 2 - Vec3(0, 0, FOCAL_LENGTH);
     }
 
     Ray getRay(double u, double v) const
